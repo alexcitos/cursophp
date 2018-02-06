b5a3e2c364d3d091e4790a5f56c1e22c9cf4d2d9
Alex Rodriguez P <alex_rope@alexrope>
2018-02-06 10:29:54 -0500

    Primer commit del curso PHP

---
+++ admin/insert-post.php
@@ -0,0 +1,62 @@
+<?php
+$result = false;
+
+if (!empty($_POST)) {
+  $sql = 'INSERT INTO blog_posts (title, content) VALUES (:title, :content)';
+  $query = $pdo->prepare($sql);
+  $result = $query->execute([
+    'title' => $_POST['title'],
+    'content' => $_POST['content']
+  ]);
+}
+?>
+<html>
+  <head>
+    <title>Blog with Platzi</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Blog Title</h1>
+        </div>
+      </div>
+      <div class="row">
+          <div class="col-md-8">
+            <h2>New Post</h2>
+            <p>
+              <a class="btn btn-default" href="posts.php">Back</a>
+            </p>
+            <?php
+              if ($result) {
+                echo '<div class="alert alert-success">Post Saved!</div>';
+              }
+            ?>
+
+            <form action="insert-post.php" method="post">
+              <div class="form-group">
+                <label for="inputTitle">Title</label>
+                <input type="text" class="form-control" name="title" id="inputTitle">
+              </div>
+              <textarea class="form-control" name="content" id="inputContent" rows="5"></textarea>
+                <br>
+                <input class="btn btn-primary" type="submit" name="" value="Save">
+            </form>
+          </div>
+        <div class="col-md-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum congue erat ac arcu volutpat lobortis. Aliquam nisl risus, pellentesque eu libero a, vehicula porta enim.
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <footer>
+            This is a footer!<br>
+            <a href="admin/index.php">Admin Panel</a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ views/index.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>Blog with Platzi</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Blog Title</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <?php
+            foreach ($blogPosts as $blogPost) {
+              echo '<div class="blog-post">';
+              echo '<h2>' . $blogPost['title'] . '</h2>';
+              echo '<p>Jan 1, 2020 by <a href="#">Alex</a></p>';
+              echo '<div class="blog-post-image">';
+              echo '</div>';
+              echo '<img src="images/keyboard.png" alt="">';
+              echo '<div class="blog-post-content">';
+              echo $blogPost['content'];
+              echo '</div>';
+              echo '</div>';
+            }
+          ?>
+          </div>
+        <div class="col-md-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum congue erat ac arcu volutpat lobortis. Aliquam nisl risus, pellentesque eu libero a, vehicula porta enim.
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <footer>
+            This is a footer!<br>
+            <a href="admin/index.php">Admin Panel</a>
+          </footer>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
