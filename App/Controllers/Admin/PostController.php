f4870168cd25388d3d938770ae6c6fe4e4beef23
alexcitos19 <alitos.rope@gmail.com>
2018-02-07 16:01:08 -0500

    Aplicando Eloquent

--- App/Controllers/Admin/PostController.php
+++ App/Controllers/Admin/PostController.php
@@ -1,34 +1,29 @@
 <?php
 namespace App\Controllers\Admin;
 
-class PostController {
+use App\Controllers\BaseController;
+use App\Models\BlogPost;
+
+class PostController extends BaseController {
   public function getIndex() {
-    global $pdo;
-
-    $query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
-    $query->execute();
-
-    $blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
-    return render('../views/admin/posts.php', ['blogPosts' => $blogPosts]);
+    $blogPosts = BlogPost::all();
+    return $this->render('admin/posts.twig', ['blogPosts' => $blogPosts]);
 
   }
 
   public function getCreate() {
-    return render('../views/admin/insert-post.php');
+    return $this->render('admin/insert-post.twig');
 
   }
 
   public function postCreate() {
-    global $pdo;
-
-    $sql = 'INSERT INTO blog_posts (title, content) VALUES (:title, :content)';
-    $query = $pdo->prepare($sql);
-    $result = $query->execute([
+    $blogPost = new BlogPost([
     'title' => $_POST['title'],
     'content' => $_POST['content']
     ]);
+    $blogPost->save();
+    $result = true;
 
-    return render('../views/admin/insert-post.php', ['result' => $result]);
-
+    return $this->render('admin/insert-post.twig', ['result' => $result]);
   }
 }
